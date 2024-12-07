5284b9a2b4e5d26bdb4a282b37b022de5a95ce22
Elvis Morales Campoverde <ecampo12@gmail.com>
2024-12-06 22:08:59 -0500

    day 5 in Go complete

--- Go/day05/AOC.go
+++ Go/day05/AOC.go
@@ -7,58 +7,126 @@ import (
 	"strings"
 )
 
-func parseInput(input string) (map[int][]int, [][]int) {
-	s := strings.Split(input, "\n\n")
-	rules := map[int][]int{}
-	for _, rule := range strings.Split(s[0], "\n") {
-		p := strings.Split(rule, "|")
-		a, _ := strconv.Atoi(p[0])
-		b, _ := strconv.Atoi(p[1])
-		if rules[a] == nil {
-			rules[a] = []int{}
-		}
-		rules[a] = append(rules[a], b)
-	}
-	updates := make([][]int, len(strings.Split(s[1], "\n")))
-	for i, line := range strings.Split(s[1], "\n") {
-		update := []int{}
-		for _, u := range strings.Split(line, ",") {
-			num, _ := strconv.Atoi(u)
-			update = append(update, num)
-		}
-		updates[i] = update
-	}
-	return rules, updates
+type Rule struct {
+	a string
+	b string
 }
-func valid(rules map[int][]int, update []int) bool {
-	updateIndex := map[int]int{}
-	for i, u := range update {
-		updateIndex[u] = i
+
+type Update struct {
+	pages []string
+	rules []Rule
+}
+
+func (u *Update) isValid() bool {
+	indexUpdate := map[string]int{}
+	for i, p := range u.pages {
+		indexUpdate[p] = i
 	}
 
-	// for _, u := range update {
-
-	// }
+	for _, rule := range u.rules {
+		if indexUpdate[rule.a] > indexUpdate[rule.b] {
+			return false
+		}
+	}
 	return true
 }
 
+// Topological sort of the pages using the rules and using the Kahn's algorithm.
+// I checked, there is no cycle in the graph, so the topological sort is unique.
+// https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm
+func (u *Update) topologicalSort() []string {
+	graph := map[string][]string{}
+	inDegree := map[string]int{}
+
+	nodes := map[string]bool{}
+	for _, rule := range u.rules {
+		if _, ok := graph[rule.a]; !ok {
+			graph[rule.a] = []string{}
+		}
+		graph[rule.a] = append(graph[rule.a], rule.b)
+		inDegree[rule.b]++
+		nodes[rule.a] = true
+		nodes[rule.b] = true
+	}
+
+	for node := range nodes {
+		if _, ok := inDegree[node]; !ok {
+			inDegree[node] = 0
+		}
+	}
+
+	queue := []string{}
+	sorted := []string{}
+	for node, degree := range inDegree {
+		if degree == 0 {
+			queue = append(queue, node)
+		}
+	}
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		sorted = append(sorted, node)
+		for _, neighbor := range graph[node] {
+			inDegree[neighbor]--
+			if inDegree[neighbor] == 0 {
+				queue = append(queue, neighbor)
+			}
+		}
+	}
+	return sorted
+}
+
+func parseInput(input string) []Update {
+	updates := []Update{}
+	s := strings.Split(input, "\n\n")
+	rules := []Rule{}
+	for _, rule := range strings.Split(s[0], "\n") {
+		p := strings.Split(rule, "|")
+		rules = append(rules, Rule{p[0], p[1]})
+	}
+	for _, line := range strings.Split(s[1], "\n") {
+
+		update := []string{}
+		set := map[string]bool{}
+		for _, u := range strings.Split(line, ",") {
+			update = append(update, u)
+			set[u] = true
+		}
+		// Find the rules that are applicable to this update
+		localRules := []Rule{}
+		for _, rule := range rules {
+			if set[rule.a] && set[rule.b] {
+				localRules = append(localRules, rule)
+			}
+		}
+		updates = append(updates, Update{update, localRules})
+	}
+	return updates
+}
+
 func part1(input string) int {
-	rules, updates := parseInput(input)
+	updates := parseInput(input)
 	sum := 0
 	for _, update := range updates {
-		localRules := make(map[int][]int)
-		for _, r := range update {
-			localRules[r] = rules[r]
-		}
-		if valid(localRules, update) {
-			sum += update[len(update)/2]
+		if update.isValid() { // we can use the topological sort to check if the update is valid, not sure if it is faster
+			mid, _ := strconv.Atoi(update.pages[len(update.pages)/2])
+			sum += mid
 		}
 	}
 	return sum
 }
 
 func part2(input string) int {
-	return 0
+	updates := parseInput(input)
+	sum := 0
+	for _, update := range updates {
+		if !update.isValid() {
+			mid, _ := strconv.Atoi(update.topologicalSort()[len(update.pages)/2])
+			sum += mid
+		}
+	}
+	return sum
 }
 
 func main() {
